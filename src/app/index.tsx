25c8e8e9ea74daacc68f5cbba688336faebcb41d
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-07-13 21:50:09 -0300

    fix: configure NativeWind for Expo Router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,9 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native'
 
 export default function HomeScreen() {
 	return (
-		<View className="h-screen bg-zinc-950 p-6">
+		<View className="flex-1 bg-zinc-950 p-6">
 			<Text className="text-4xl font-bold text-zinc-50">Your </Text>
 		</View>
-	);
+	)
 }

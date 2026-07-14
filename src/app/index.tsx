585d3ed1402007860dcaecb6d905f6e8a7821709
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-07-13 22:28:11 -0300

    chore: add ESLint config and format app setup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,10 @@
-import { Text, View } from 'react-native'
+import { StatusBar, Text, View } from 'react-native'
 
 export default function HomeScreen() {
-	return (
-		<View className="flex-1 bg-zinc-950 p-6">
-			<Text className="text-4xl font-bold text-zinc-50">Your </Text>
-		</View>
-	)
+  return (
+    <View className="flex-1 bg-zinc-950 items-center justify-center">
+      <Text className="text-5xl font-bold text-zinc-50">Your time capsule</Text>
+      <StatusBar barStyle="light-content" translucent />
+    </View>
+  )
 }

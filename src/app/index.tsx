7fc5d64e4270732309587dac5d8b74f7eecc11b5
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-07-13 22:37:45 -0300

    chore: align Prettier with ESLint formatting

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { StatusBar, Text, View } from 'react-native'
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 bg-zinc-950 items-center justify-center">
+    <View className="flex-1 items-center justify-center bg-zinc-950">
       <Text className="text-5xl font-bold text-zinc-50">Your time capsule</Text>
       <StatusBar barStyle="light-content" translucent />
     </View>

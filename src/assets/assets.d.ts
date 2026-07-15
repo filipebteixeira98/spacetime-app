9b01d213671a4fe7beb75c77c9155040792def45
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-07-15 13:12:39 -0300

    chore(assets): configure SVG asset imports for React Native

--- src/assets/assets.d.ts
+++ src/assets/assets.d.ts
@@ -1,1 +1,10 @@
 declare module '*.png'
+
+declare module '*.svg' {
+  import type React from 'react'
+  import type { SvgProps } from 'react-native-svg'
+
+  const content: React.FC<SvgProps>
+
+  export default content
+}

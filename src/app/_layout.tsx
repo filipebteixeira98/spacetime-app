be755a2f9272723437ae7e59b352b7eb6b415b7e
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-07-15 12:25:29 -0300

    feat(layout): load custom fonts before rendering app stack

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,28 @@
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto'
+import { Stack } from 'expo-router'
+import * as SplashScreen from 'expo-splash-screen'
+
 import '../global.css'
 
-import { Stack } from 'expo-router'
-
 export default function RootLayout() {
+  SplashScreen.preventAutoHideAsync()
+
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
+  SplashScreen.hideAsync()
+
   return <Stack screenOptions={{ headerShown: false }} />
 }

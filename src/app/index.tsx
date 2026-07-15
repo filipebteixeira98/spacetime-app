1463f4952b34aa8db2e1a7c25c20fa7a75f5769d
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-07-15 14:48:18 -0300

    feat(home): build branded time capsule landing screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,52 @@
-import { StatusBar, Text, View } from 'react-native'
+import { cssInterop } from 'nativewind'
+import {
+  ImageBackground,
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+
+import blurBackground from '../assets/bg-blur.png'
+import Logo from '../assets/nlw-spacetime-logo.svg'
+import Stripes from '../assets/stripes.svg'
+
+const StyledStripes = cssInterop(Stripes, {
+  className: 'style',
+})
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-zinc-950">
-      <Text className="text-5xl font-bold text-zinc-50">Your time capsule</Text>
+    <ImageBackground
+      source={blurBackground}
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStripes className="absolute left-2" />
+      <View className="flex-1 items-center justify-center gap-6">
+        <Logo />
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Your time capsule
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Collect memorable moments from your journey and share them (if you
+            wish) with the world!
+          </Text>
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Set a reminder
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Made with 💕
+      </Text>
       <StatusBar barStyle="light-content" translucent />
-    </View>
+    </ImageBackground>
   )
 }
